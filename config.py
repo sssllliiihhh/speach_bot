97b3f2365e00da04e6b914a24c6df8d8c7136e5f
sssllliiihhh <lihobabinslavik93@gmail.com>
2024-05-13 02:40:31 +0300

    1

---
+++ config.py
@@ -0,0 +1,16 @@
+TELEGRAM_TOKEN = ''
+FOLDER_ID = ''
+IAM_TOKEN = ''
+COUNT_LAST_MSG = 4
+MAX_USER_TTS_SYMBOLS = 1000
+MAX_TTS_SYMBOLS = 200
+MAX_USER_STT_BLOCKS = 12
+MAX_USERS = 3
+MAX_GPT_TOKENS = 120
+MAX_USER_GPT_TOKENS = 2_000
+SYSTEM_PROMPT = [{'role': 'system',
+                  'text': 'Ты умный и дружелюбный собеседник. '
+                          'Общайся с пользователем на "ты" и используй юмор.'
+                          'Поддерживай диалог. Изображай человека'}]
+LOGS = 'logs.txt'
+PATH_TO_DB = 'messages.db'
